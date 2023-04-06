03ab8d6a7149fc1cd9df3d07f530244763a455c1
PeterhdPham <peterhdpham@gmail.com>
2023-04-06 18:37:36 +0200

    ny?

--- projectfolder/static/function.js
+++ projectfolder/static/function.js
@@ -1,33 +1,27 @@
 $(function () {
   $("a#button").on("click", function (e) {
     e.preventDefault();
-    $.getJSON("/compare", function (data) {
-      readFile();
-    });
+    readFile(); // Move readFile() inside the click event handler
+    return false;
+  });
+
+  $("a#stop").on("click", function (e) {
+    e.preventDefault();
+    // Add code for stopping the image updates, if needed
     return false;
   });
 });
 
 function readFile() {
-  jQuery.get('result.txt', function (txt) {
+  $.get('/result.txt', function (txt) {
     $('#outputBx').text(txt);
-    refreshImage('/image.jpg'); // Pass the image path as a parameter
+    refreshImage(); // Call refreshImage() inside the success callback
   });
 }
 
-function refreshImage(imagePath) {
+function refreshImage() {
   var timestamp = new Date().getTime();
-  jQuery.get(imagePath, function() {
-    $('#re').attr('src', imagePath);
-  });
+  var imagePath = '/image.jpg?' + timestamp; // Add a timestamp as a query parameter to the image URL to prevent caching
+  $('#re').attr('src', imagePath); // Update the image source with the new URL
 }
 
-$(function () {
-  $("a#stop").on("click", function (e) {
-    e.preventDefault();
-    $.getJSON("/stop", function (data) {
-
-    });
-    return false;
-  });
-});
